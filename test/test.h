166c07b1a0e1bb43f697e71c573b08bbf455b550
Vitold Kapshitzer <vitold@10-sor.com>
2017-10-27 02:37:37 +0200

    test update task: MILA-8 bug: double free fix bug: MILA-9 bug in curl fix

--- test/test.h
+++ test/test.h
@@ -2,6 +2,8 @@
 #define _TEST_H
 
 #include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 #define PRINT_LINE_N printf("test failed at line: %d\n", __LINE__);
 #define TEST_CHECK(x) (if(x){} else {PRINT_LINE_N;return 1;})
@@ -17,4 +19,43 @@
 #define TEST_FAIL return 1;
 #define FAIL(x)
 
+
+
+
+
+int fill_buffer_from_file(char *filename, char **lbuffer)
+{
+  FILE * pFile;
+  long lSize;
+  size_t result;
+  pFile = fopen ( filename , "rb" );
+  if (pFile==NULL) {fputs ("File error\n",stderr); return (1);}
+
+  // obtain file size:
+  fseek (pFile , 0 , SEEK_END);
+  lSize = ftell (pFile);
+  rewind (pFile);
+
+  // allocate memory to contain the whole file:
+  char *buffer = (char*) malloc (sizeof(char)*lSize+1);
+  if (buffer == NULL) {fputs ("Memory error\n",stderr); return (2);}
+
+  // copy the file into the buffer:
+  result = fread (buffer,1,lSize,pFile);
+  if (result != lSize) {fputs ("Reading error\n",stderr); return (3);}
+
+  buffer[sizeof(char)*lSize] = 0;
+
+  /* the whole file is now loaded in the memory buffer. */
+  // terminate
+  fclose (pFile);
+
+  *lbuffer = buffer;
+
+  return 0;
+
+}
+
 #endif
+
+
