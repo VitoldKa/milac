7d286c05b1564921bdd59d6af98a555f5ac2a84f
Vitold Kapshitzer <vitold@10-sor.com>
2017-12-13 23:49:50 +0100

    fix bugs fix memory leak add multi user add use of mila API

--- include/mila.h
+++ include/mila.h
@@ -8,12 +8,21 @@ extern pthread_mutex_t mutex;
 
 typedef struct {
 	char *email;
+	char *bearer_key;
 	char *credentials;
 	char *cookiejar;
+	char *forward;
+
 	CURL *curl;
+	CURL *curl_api;
+	struct curl_slist *header_api_list;
+	struct curl_slist *list;
+	
+	pthread_mutex_t mutex;
 } s_mila_profile;
 
 int mila_init(void);
+int mila_free(void);
 int mila (char *inbuf, int size, char *to);
 
 int isemailexist(char*);
